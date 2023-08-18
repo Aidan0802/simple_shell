91331604734fe1a133acaa89e95ea823f9ae9461
LeighAnne17 <leighndimande17@icloud.com>
2023-08-19 01:51:50 +0200

    [update]c function

--- setenv.c
+++ setenv.c
@@ -13,22 +13,28 @@ int _setenv(char **argv)
 {
 	if (argv[1] == NULL || argv[2] == NULL)
 	{
-		dprint(stderr, "could not find environment variable.\n");
-	return (-1);
-
-}
-
-if (strcmp(argv[0], "setenv") == 0)
-{ 
-	if (setenv(av,val,1) == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		dprintf(fileno(stderr), "modify environment of variable.\n");
+		dprintf(stderr, "Arguments missing.\n");
 		return (-1);
 	}
+
+
+	if (strcmp(argv[0], "setenv") == 0)
+	{ 
+		if (setenv(argv[1], argv[2],1) == 0)
+		{
+
+			return (0);
+		}
+		else
+		{		
+			dprintf(fileno(stderr), "Could not modify environment of variable.\n");
+			return (-1);
+		}	
+	}
+	dprintf(fileno(stderr), "Command not recognized.\n");
+	return (-1);
+	
+	
 }
 
 
