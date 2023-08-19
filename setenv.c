027f533970faabc3008b6e461524b7225147ac1d
aidan <aidanheydenrycht@gmail.com>
2023-08-19 11:43:04 +0300

    [Create] C file function

--- setenv.c
+++ setenv.c
@@ -1,40 +1,36 @@
 #include "shell.h"
-ude <stdio.>
-
-#include <string.h>
 
 /**
  * _setenv - set/unset envronment variables
  * @argv: argument address
- * Return: 0 upon success or -1 upon failure
+ * Return: 0 (found), -1 (not found)
  */
 
 int _setenv(char **argv)
 {
-	if (argv[1] == NULL || argv[2] == NULL)
-	{
-		dprintf(stderr, "Arguments missing.\n");
-		return (-1);
-	}
-
-
 	if (strcmp(argv[0], "setenv") == 0)
-	{ 
-		if (setenv(argv[1], argv[2],1) == 0)
+	{
+		if (argv[1] == NULL || argv[2] == NULL)
 		{
-
+			dprintf(STDERR_FILENO, "Usage: setenv VARIABLE VALUE.\n");
 			return (0);
 		}
-		else
-		{		
-			dprintf(fileno(stderr), "Could not modify environment of variable.\n");
-			return (-1);
-		}	
+		else if (setenv(argv[1], argv[2], 1) == 0)
+			return (0);
+		dprintf(STDERR_FILENO, "Could not set environment of variable.\n");
+		return (0);
 	}
-	dprintf(fileno(stderr), "Command not recognized.\n");
+	if (strcmp(argv[0], "unsetenv") == 0)
+	{
+		if (argv[1] == NULL)
+		{
+			dprintf(STDERR_FILENO, "Usage: setenv VARIABLE.\n");
+			return (0);
+		}
+		else if (unsetenv(argv[1]) == 0)
+			return (0);
+		dprintf(STDERR_FILENO, "Could not unset environment of variable.\n");
+		return (0);
+	}
 	return (-1);
-	
-	
 }
-
-
