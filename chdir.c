7a550e42f9f16ae4b3894b76a2592ae89b401ad1
aidan <aidanheydenrycht@gmail.com>
2023-08-25 11:54:42 +0300

    [Fix] removed print

--- chdir.c
+++ chdir.c
@@ -21,12 +21,14 @@ int _chdir(char **av, int cmd_count)
 		if (av[1] == NULL)
 		{
 			getcwd(prev, BUF_SIZE);
+			printf("%s", home);
 			chdir(home);
 			return (0);
 		}
 		if (_strcmp(av[1], "-") == 0)
 		{
 			getcwd(prev, BUF_SIZE);
+			printf("%s", temp);
 			chdir(temp);
 			return (0);
 		}
@@ -39,6 +41,7 @@ int _chdir(char **av, int cmd_count)
 				fprintf(stderr, "./hsh: %d: %s: Not found\n", cmd_count, av[1]);
 				return (0);
 			}
+			printf("%s", av[1]);
 			return (0);
 		}
 	}
