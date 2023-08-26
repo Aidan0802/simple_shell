986c2f94c7deebc463c22702255f03646a6167b7
aidan <aidanheydenrycht@gmail.com>
2023-08-26 03:53:32 +0300

    [Fix] output

--- seperators.c
+++ seperators.c
@@ -5,31 +5,27 @@ int handle_commands(char *commands, int cmd_count)
     int exit_status = 0;
     char *token = strtok(commands, ";");
     char *trimmed_token;
-    char **argv;
+    char *argv[100];
     int argc = 0;
     char *cmd_token;
 
-    while (token != NULL) {
+    while (token) 
+    {
         trimmed_token = token;
 
-        while (*trimmed_token == ' ' || *trimmed_token == '\t')
-            trimmed_token++;
-
-        if (*trimmed_token != '\0')
+        while (*trimmed_token)
 	{
-            argv = malloc(100 * sizeof(char *));
             cmd_token = strtok(trimmed_token, " \n");
 
             while (cmd_token != NULL)
 	    {
-                *argv[argc++] = *cmd_token;
+                argv[argc++] = cmd_token;
                 cmd_token = strtok(NULL, " \n");
             }
             argv[argc] = NULL;
 
             exit_status = exec_cmd(argv, argv[0], cmd_count, &commands);
 
-            free(argv);
         }
 
         token = strtok(NULL, ";");
